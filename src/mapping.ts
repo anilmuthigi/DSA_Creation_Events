7a022f4ecce96a18b8f3d62b53375e97a3214f3a
anilmuthigi <54994968+anilmuthigi@users.noreply.github.com>
2022-02-14 09:32:04 +0530

    Added DSA account creation SubGraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,16 +1,8 @@
 import {
   LogAccountCreated as LogAccountCreatedEvent,
-  LogNewAccount as LogNewAccountEvent,
-  LogNewCheck as LogNewCheckEvent,
-  LogNewMaster as LogNewMasterEvent,
-  LogUpdateMaster as LogUpdateMasterEvent
 } from "../generated/Token/Token"
 import {
-  LogAccountCreated,
-  LogNewAccount,
-  LogNewCheck,
-  LogNewMaster,
-  LogUpdateMaster
+  LogAccountCreated
 } from "../generated/schema"
 
 export function handleLogAccountCreated(event: LogAccountCreatedEvent): void {
@@ -21,40 +13,5 @@ export function handleLogAccountCreated(event: LogAccountCreatedEvent): void {
   entity.owner = event.params.owner
   entity.account = event.params.account
   entity.origin = event.params.origin
-  entity.save()
-}
-
-export function handleLogNewAccount(event: LogNewAccountEvent): void {
-  let entity = new LogNewAccount(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity._newAccount = event.params._newAccount
-  entity._connectors = event.params._connectors
-  entity._check = event.params._check
-  entity.save()
-}
-
-export function handleLogNewCheck(event: LogNewCheckEvent): void {
-  let entity = new LogNewCheck(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.accountVersion = event.params.accountVersion
-  entity.check = event.params.check
-  entity.save()
-}
-
-export function handleLogNewMaster(event: LogNewMasterEvent): void {
-  let entity = new LogNewMaster(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.master = event.params.master
-  entity.save()
-}
-
-export function handleLogUpdateMaster(event: LogUpdateMasterEvent): void {
-  let entity = new LogUpdateMaster(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.master = event.params.master
   entity.save()
 }
